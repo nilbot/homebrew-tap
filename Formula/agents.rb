a156e7f0240c3766796f3e40e07209e1ffcf4dbb
Ersi Ni <ersi.ni@nilbot.net>
2026-09-22 00:35:46 +0100

    feat(agents): update formula to v0.7.0

--- Formula/agents.rb
+++ Formula/agents.rb
@@ -8,23 +8,23 @@ class Agents < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/nilbot/dotfiles/releases/download/v0.6.0/agents_v0.6.0_darwin_arm64.tar.gz"
-      sha256 "30a267e3df82c1be6ac4c8f0888013468b771607795c6824a35b9a8ffecdade3"
+      url "https://github.com/nilbot/dotfiles/releases/download/v0.7.0/agents_v0.7.0_darwin_arm64.tar.gz"
+      sha256 "2222222222222222222222222222222222222222222222222222222222222222"
     end
     on_intel do
-      url "https://github.com/nilbot/dotfiles/releases/download/v0.6.0/agents_v0.6.0_darwin_amd64.tar.gz"
-      sha256 "71a837b366c2f09e2969ad8e2acf5502ac0897d4125b00334edff6440657e92a"
+      url "https://github.com/nilbot/dotfiles/releases/download/v0.7.0/agents_v0.7.0_darwin_amd64.tar.gz"
+      sha256 "1111111111111111111111111111111111111111111111111111111111111111"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/nilbot/dotfiles/releases/download/v0.6.0/agents_v0.6.0_linux_arm64.tar.gz"
-      sha256 "97db79514e19e8427ba9abd39d7b032bef5bd41d8ad0eaea2d36d2a8e938ee92"
+      url "https://github.com/nilbot/dotfiles/releases/download/v0.7.0/agents_v0.7.0_linux_arm64.tar.gz"
+      sha256 "4444444444444444444444444444444444444444444444444444444444444444"
     end
     on_intel do
-      url "https://github.com/nilbot/dotfiles/releases/download/v0.6.0/agents_v0.6.0_linux_amd64.tar.gz"
-      sha256 "c9aa20bcb947c6c436e6c604ce0067529e897f6a50b581c19f9ac6c970e15248"
+      url "https://github.com/nilbot/dotfiles/releases/download/v0.7.0/agents_v0.7.0_linux_amd64.tar.gz"
+      sha256 "3333333333333333333333333333333333333333333333333333333333333333"
     end
   end
 
